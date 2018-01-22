7b7b64f17a4161d92fa04bdf22081ca736251984
wnwo9037 <wnwo9037@gmail.com>
2018-01-22 23:18:13 +0900

    Follow PEP8 style guide.

--- PythonBaseStudy/study/algorithm/searchNormal/CabToBat.py
+++ PythonBaseStudy/study/algorithm/searchNormal/CabToBat.py
@@ -7,34 +7,36 @@ graph["CAT"] = ["MAT", "BAT"]
 graph["BAR"] = ["BAT"]
 graph["MAT"] = ["BAT"]
 
-def findWay(name, goal):
+
+def find_way(name, goal):
     find_queue = deque()
     find_queue += graph[name]
-    edgeCount = {}
-    edgeCount[name] = 0
-    beforeTag = {}
+    edge_count = {}
+    edge_count[name] = 0
+    before_tag = {}
     visited = []
     for nextWord in graph[name]:
-        edgeCount[nextWord] = edgeCount[name] + 1
-        beforeTag[nextWord] = name
+        edge_count[nextWord] = edge_count[name] + 1
+        before_tag[nextWord] = name
         visited.append(nextWord)
     while find_queue:
         word = find_queue.popleft()
         if is_word(word, goal):
             print("Found BAT")
-            return edgeCount[word]
+            return edge_count[word]
         else:
             print("NOT YET REACHED BAT. CURRENT: " + word)
             visited.append(word)
             for nextWord in graph[word]:
-                if not nextWord in visited:
-                    edgeCount[nextWord] = edgeCount[word] + 1
-                    beforeTag[nextWord] = word
+                if nextWord not in visited:
+                    edge_count[nextWord] = edge_count[word] + 1
+                    before_tag[nextWord] = word
                     visited.append(nextWord)
                 find_queue += graph[word]
     return False
 
+
 def is_word(name, goal):
     return name == goal
 
-print(findWay("CAB", "BAT"))
+print(find_way("CAB", "BAT"))
